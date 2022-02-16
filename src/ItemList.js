422c278f56b13923eddc19f6ea237fa4f8f51b5b
Nastya <98601803+NastyaKaas@users.noreply.github.com>
2022-02-16 22:39:51 +0800

    main functionality added

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,10 +1,17 @@
- function ListItem({item}) {
+ import {MdDelete, MdEdit} from "react-icons/md";
+
+ function ListItem({item, editItem, deleteItem}) {
      return (
-         <div>   
-             {console.log(item)} 
+         <div className="items-list">   
            {item.map(oneItem => (
                <div key={oneItem.id}>
+               <div className="button-item-container"> 
                <p>{oneItem.addedItem}</p>
+               <div className="button-container">
+                   <div className="delete-button" onClick={() => deleteItem(oneItem.id)}><MdDelete/></div>
+                   <div className="edit-button"  onClick={() => editItem(oneItem.id)}><MdEdit /></div>
+                </div>    
+               </div>
                </div>
            ))}
          </div>
--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,11 +1,14 @@
-import { useState } from "react"; 
+import { useEffect, useState } from "react"; 
 import ItemList from "./ItemList";
 
 function ShoppingList () {
 
     let [item, setItem] = useState([]);
-
     let [userInput, setUserInput] = useState("");
+    let [editing, setEditing] = useState(false);
+    let [deleting, setDeleting] = useState(false);
+    let [id, setId] = useState(null);
+    let [messageForUser, setMessageForUser] = useState(false);
 
     let addNewItem = (addedItem) => {
         let id = Math.floor(Math.random() * 1000) + 1;
@@ -17,7 +20,51 @@ function ShoppingList () {
 
     let handleSubmit = (e) => {
         e.preventDefault();
-        addNewItem(userInput);
+        if(userInput) {
+            addNewItem(userInput); 
+        }
+        if(editing && userInput) {
+             setItem(item.map(oneItem => {
+                 if(oneItem.id === id) {
+                  return {...oneItem, addedItem: userInput}; 
+                }
+                return oneItem;
+              }))
+        }
+        setUserInput("");
+        setEditing(false);
+    }
+
+    useEffect(() => {
+        let showMessage = setTimeout(() => {
+            setMessageForUser(true);
+        },100)
+        return () => {
+            clearTimeout(showMessage)
+        }
+    },[userInput])
+
+    let deleteItem = (id) => {
+      let filteredItems = item.filter(oneItem => { 
+        return oneItem.id !== id
+    });  
+        setItem(filteredItems);
+        setDeleting(true);
+    }
+
+    let editItem = (id) => {
+        item.forEach(oneItem => { 
+            if(oneItem.id === id) {
+                setUserInput(oneItem.addedItem);
+                setId(oneItem.id);
+                setEditing(true);
+            }
+        })
+    }
+
+    let clearAll = () => {
+        setItem([]);
+        setDeleting(true);
     }
 
     return (
@@ -29,9 +76,10 @@ function ShoppingList () {
                 placeholder="Введите название продукта"
                 value={userInput}
                 onChange={(e) => {setUserInput(e.target.value)}}/>
-                <button type="submit">Добавить</button>
+                {editing ? <button type="submit">Редактировать</button> : <button type="submit">Добавить</button>}
             </form> 
-            {item && <ItemList item={item}/>} 
+            {item && <ItemList item={item} deleteItem={deleteItem} editItem={editItem}/>}
+            {item.length > 0 && <button onClick={clearAll}>Очистить Все</button>}
         </section>
     )
 }
